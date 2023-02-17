8d1c3cfb0f5f678db6a7246b371300705370930a
Michal <mbaka0002@gmail.com>
2023-02-17 14:34:33 +0100

    Add tests for AppUserService class

---
+++ src/main/java/anstart/gokarty/configuration/AppConfiguration.java
@@ -0,0 +1,16 @@
+package anstart.gokarty.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+
+}
